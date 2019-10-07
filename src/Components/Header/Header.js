dc857e213379b2cc58ccbd978c12872e7c3c1137
AgonIdrizi <agon.88@hotmail.com>
2019-10-07 17:34:19 +0200

    add nav-content in header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
-import "./Header.css";
+import "./Header.scss";
 const Header = () => {
-  return <header className='Header'></header>;
+  return (
+    <header className="Header">
+      <div className="NavContent">
+        <h2>Food IN</h2>
+        <ul>
+          <li>All Restaurants</li>
+          <li>Login</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
