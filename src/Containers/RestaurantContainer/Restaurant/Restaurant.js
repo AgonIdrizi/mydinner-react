78c85c70cd0b1aeab7e897f4146b0103986fe1a3
AgonIdrizi <agon.88@hotmail.com>
2020-05-08 14:28:44 +0200

    add useScrollPosition hook

--- src/Containers/RestaurantContainer/Restaurant/Restaurant.js
+++ src/Containers/RestaurantContainer/Restaurant/Restaurant.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React , { useRef, useState } from 'react';
 import ItemCard from "./ItemCard/ItemCard";
-import { useInView } from 'react-intersection-observer'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 import "./Restaurant.scss";
 import RestaurantImage from "../../../assets/restaurant/restaurantImage.jpeg";
 
 const Restaurant = ({ resData }) => {
-  const [ref, inView, entry] = useInView({
-    /* Optional options */
-    //threshold: 0.60
-    threshold: 0.63
+  
+  const [categoriesStyle, setCategoriesStyle] = useState({
+    
   })
+
+  const categoriesRef = useRef(null)
+  const cartRef =  useRef(null)
+  
+useScrollPosition(({ prevPos, currPos }) => {
+  console.log(currPos.x)
+  console.log(currPos.y)
+  console.log('categoriesRef', categoriesRef)
+  if(currPos.y< -390) {
+    categoriesRef.current.style.backgroundColor = 'red'
+    setCategoriesStyle({
+      backgroundColor: 'red',
+      position: 'fixed',
+      top: '20px',
+      bottom: 'auto',
+      left: '80px',
+      right: 'auto'
+    })
+  } else {
+    setCategoriesStyle({})
+  }
+})
+
+
   return (
-    <div   className="RestaurantContainer">
-      <div className="Restaurant">
-        <div className="RestaurantHeader">
+    <div className="RestaurantContainer">
+      <div  className="Restaurant">
+        <div  className="RestaurantHeader">
           <h2>{resData.restaurantName}</h2>
           <img src={RestaurantImage} />
           <div className="RestaurantInfos">
@@ -22,15 +45,22 @@ const Restaurant = ({ resData }) => {
             <span>{resData.restaurantContact}</span>
           </div>
         </div>
-        <h2>{`Header inside viewport ${inView}.`}</h2>
-        <div ref={ref}   className="RestaurantMenu">
+        <h2>{`Header inside viewport .`}</h2>
+        <div className="RestaurantMenu">
+          <section ref={categoriesRef} style={categoriesStyle} className="Categories">
+            <div>sdfsdfsdf</div>
+            <div>sdfsdfsdf</div>
+            <div>sdfsdfsdf</div>
+            <div>sdfsdfsdf</div>
+            <div>sdfsdfsdf</div>
+            <div>sdfsdfsdf</div>
+          </section>
           <section className="Menus">
             <ItemCard />
             <ItemCard />
             <ItemCard />
             <ItemCard />
             <ItemCard />
-           <h2>{`${inView}`}</h2> 
             <ItemCard />
             <ItemCard />
             <ItemCard />
