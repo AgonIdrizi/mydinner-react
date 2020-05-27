e98df87350a07fa57eac404b1cb6ef03c22e80c8
AgonIdrizi <agon.88@hotmail.com>
2020-05-27 18:54:26 +0200

    add useEffect in Cart component

--- src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
+++ src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
@@ -1,15 +1,33 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from "react";
 import "./CartCard.scss";
 import { CartContext } from "../../../../contexts/CartContext";
 
 const CartCard = () => {
-  const context = useContext(CartContext)
+  const context = useContext(CartContext);
   const { itemsInCart } = context;
-  console.log("agon", itemsInCart)
+  const displayItemsObj = {};
+
+  useEffect(() => {
+    itemsInCart.map(cartObj => {
+      displayItemsObj[cartObj.name] === undefined
+        ? (displayItemsObj[cartObj.name] = 1)
+        : displayItemsObj[cartObj.name]++;
+    });
+  }, [itemsInCart]);
+  console.log(displayItemsObj);
+
+  console.log("agon", itemsInCart);
   return (
     <div className="CartCard">
       <h2>Cart</h2>
       <p>{itemsInCart.length}</p>
+      <div>
+        {Object.keys(displayItemsObj).map(item => (
+          <p>
+          {item} : {displayItemsObj[item].value}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
