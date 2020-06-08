351a5491b0354a685e9021fa6cf32177c7eed179
AgonIdrizi <agon.88@hotmail.com>
2020-06-08 16:49:32 +0200

    add removeHandler for cart items

--- src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
+++ src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
@@ -6,21 +6,27 @@ import { countObjectsWithEqualProperty } from "../../../../utils/helperFunctions
 const CartCard = () => {
   const [displayItemsObj, setDisplayItemsObj] = useState({});
   const context = useContext(CartContext);
-  const { itemsInCart } = context;
+  const { itemsInCart, onRemoveMenuHandler } = context;
 
   useEffect(() => {
+    if (itemsInCart.length === 0) {
+      setDisplayItemsObj({});
+      return;
+    }
     setDisplayItemsObj(countObjectsWithEqualProperty(itemsInCart));
   }, [itemsInCart]);
 
   return (
     <div className="CartCard">
       <h2>Cart</h2>
-      <p>{itemsInCart.length}</p>
       <div>
         {Object.keys(displayItemsObj).map(item => (
-          <p>
-            {item} : {displayItemsObj[item]}
-          </p>
+          <div>
+            <p>
+              {item} : {displayItemsObj[item]}
+            </p>
+            <button onClick={() => onRemoveMenuHandler(item)}>-</button>
+          </div>
         ))}
       </div>
     </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -3,18 +3,21 @@ import React, { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartContextProvider = props => {
-  const [itemsInCart, setItemsInCart] = useState([])
+  const [itemsInCart, setItemsInCart] = useState([]);
 
   const onAddMenuHandler = item => {
     setItemsInCart([...itemsInCart, item]);
-    console.log("addMenu")
-  }
+  };
 
-  const onRemoveMenuHandler = id => {
-    console.log("removeMenu")
-  }
+  const onRemoveMenuHandler = item => {
+    let indexOfObject = itemsInCart.findIndex(elem => elem.name === item);
+    itemsInCart.splice(indexOfObject, 1);
+    setItemsInCart([...itemsInCart]);
+  };
   return (
-    <CartContext.Provider value={{ itemsInCart, onAddMenuHandler }}>
+    <CartContext.Provider
+      value={{ itemsInCart, onAddMenuHandler, onRemoveMenuHandler }}
+    >
       {props.children}
     </CartContext.Provider>
   );
