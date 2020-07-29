a1d84c965ac0a117644f00f35b894f6ad4b2f8f7
AgonIdrizi <agon.88@hotmail.com>
2020-07-29 15:56:51 +0200

    add no-items image to CartCard component

---
+++ src/Components/UI/LineBreak/LineBreak.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./LineBreak.scss";
+
+const LineBreak = () => {
+  return <div className="LineBreak"></div>;
+};
+
+export default LineBreak;
--- src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
+++ src/Containers/RestaurantContainer/Restaurant/CartCard/CartCard.js
@@ -1,7 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
+import path from 'path';
+import LineBreak from "../../../../Components/UI/LineBreak/LineBreak";
 import "./CartCard.scss";
 import { CartContext } from "../../../../contexts/CartContext";
 import { countObjectsWithEqualProperty } from "../../../../utils/helperFunctions";
+
+import  emptyCartImg from "../../../../assets/empty-cart.svg"
 
 const CartCard = ({ restaurantName }) => {
   const [displayItemsObj, setDisplayItemsObj] = useState({});
@@ -26,30 +30,37 @@ const CartCard = ({ restaurantName }) => {
       <div className="CartCardHeader">
         <h2>Cart</h2>
       </div>
-      <div className="CartRestaurantName">
-        <h3>{restaurantName}</h3>
-      </div>
-      <div></div>
-      <div className="LineBreak"></div>
-      <table>
-        <tbody>
-          {Object.keys(displayItemsObj).map(item => (
-            <tr>
-              <td>
-                <button onClick={() => onRemoveMenuHandler(item)}>-</button>
-                <span >{displayItemsObj[item]}</span>
-                <button onClick={() => onAddMenuHandler(item)}>+</button>
-              </td>
-              <td>
-                {item}
-              </td>
-              <td>
-                {countprice(item, displayItemsObj[item])}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {Object.keys(displayItemsObj).length === 0 ? null : (
+          <div className="CartRestaurantName">
+            <h3>{restaurantName}</h3>
+          </div>)}
+      {Object.keys(displayItemsObj).length !== 0 ? <LineBreak /> : null}
+      {Object.keys(displayItemsObj).length === 0 ? (
+        <div className="CartNoItemsImage">
+          <img src={emptyCartImg} style={{ height: "80px" }} alt="no-items" />
+          <p>There are no items in cart</p>
+        </div>
+      ) : (
+        <table>
+          <tbody>
+            {Object.keys(displayItemsObj).map(item => (
+              <tr>
+                <td>
+                  <button onClick={() => onRemoveMenuHandler(item)}>-</button>
+                  <span>{displayItemsObj[item]}</span>
+                  <button onClick={() => onAddMenuHandler(item)}>+</button>
+                </td>
+                <td>{item}</td>
+                <td>{countprice(item, displayItemsObj[item])}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {Object.keys(displayItemsObj).length !== 0 ? <LineBreak /> : null}
+      {Object.keys(displayItemsObj).length === 0 ? null : (
+        <div style={{ height: "100px" }}>Proceed to checkout</div>
+      )}
     </div>
   );
 };
