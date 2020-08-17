48bf89b68e05e6259d9da2a259a90e36bec8eebc
AgonIdrizi <agon.88@hotmail.com>
2020-08-17 16:26:21 +0200

    filter restaurants based on delivery address

--- src/Containers/RestuarantsContainer/RestaurantsContainer.js
+++ src/Containers/RestuarantsContainer/RestaurantsContainer.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import useDataApi from "../../hooks/useDataApi";
 import Restaurants from "./Restaurants/Restaurants";
 import Spinner from "../../Components/UI/Spinner/Spinner";
+import { OrderContext } from "../../contexts/OrderContext";
 import { TestApiUrls } from "../../config/testApiUrls";
+import { isEmptyObject } from "../../utils/helperFunctions";
 
 const RestaurantsContainer = () => {
   //const [isLoading, setIsLoading] = useState(false);
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
     TestApiUrls.restaurantsGet
   );
+  const orderContext = useContext(OrderContext);
+  const { orderDeliveryAddress } = orderContext;
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      //if delivery-address is empty display all-restaurants, else filter based on address-postalCode
+      if (isEmptyObject(orderDeliveryAddress)) {
+        setRestaurants(data.restaurants);
+      } else {
+        const filteredRestaurants = data.restaurants.filter(
+          elem => elem.postalCode === parseInt(orderDeliveryAddress.postalCode)
+        );
+        setRestaurants(filteredRestaurants);
+      }
+    }
+  }, [isLoading]);
 
   return (
-    <>
-      {isLoading ? <Spinner /> : <Restaurants restaurants={data.restaurants} />}
-    </>
+    <>{isLoading ? <Spinner /> : <Restaurants restaurants={restaurants} />}</>
   );
 };
 
