0cf27300148a5208626fa85f8af7e114514bdc24
AgonIdrizi <agon.88@hotmail.com>
2020-11-05 13:57:50 +0100

    fill footer with info

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import { FacebookOutlined, InstagramOutlined, TwitterOutlined, YoutubeOutlined } from '@ant-design/icons';
 import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="Footer">
-      <h2>Footer</h2>
+      <h2>My Dinner</h2>
+      <div className="FooterInfos">
+        <div className="ContactPages">
+          <Link to="/about" >About us</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="SocialIcons">
+          <span>Follow us: </span>
+          <FacebookOutlined style={{ fontSize: 24 }} />
+          <InstagramOutlined style={{ fontSize: 24 }} />
+          <TwitterOutlined style={{ fontSize: 24 }} />
+          <YoutubeOutlined style={{ fontSize: 24 }} />
+        </div>
+      </div>
+      <h4>©2020 MyDinner</h4>
     </div>
   );
 };
